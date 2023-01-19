9329ed9d7f0701be06822f8c512d99cad34d7aee
AlessandroLembo <alessandrolembo2@gmail.com>
2023-01-19 17:29:46 +0100

    create appHero and print image on page

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,31 @@
 <script>
 import AppTitle from './components/AppTitle.vue';
+import AppHero from './components/AppHero.vue';
 export default {
   data() {
     return {
     }
   },
   components: {
-    AppTitle: AppTitle,
+    AppTitle,
+    AppHero
   }
 }
 </script>
 
 <template>
-  <AppTitle />
+  <div class="first-app">
+    <app-title></app-title>
+    <app-hero></app-hero>
+  </div>
+
+
 </template>
 
 <style>
-
+.first-app {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
 </style>
---
+++ src/components/AppHero.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    name: 'AppHero',
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<template>
+    <img src="../assets/jumbo.jpg" alt="jumbo">
+</template>
+
+<style>
+
+</style>
