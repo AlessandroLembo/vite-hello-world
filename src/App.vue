59ea75d0e9f095434b068857530c8a1688fca21d
AlessandroLembo <alessandrolembo2@gmail.com>
2023-01-19 16:03:15 +0100

    first task

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  data() {
+    return {
+      name: 'Ale'
+    }
+  }
+}
+</script>
+
+<template>
+  <p>Ciao {{ name }} </p>
+</template>
+
+<style>
+p {
+  color: red;
+}
+</style>
